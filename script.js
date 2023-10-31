a05b7f4f0924c63fd0f5c520b170ae55c4f688a1
Becksensei <as.atsadayut@gmail.com>
2023-10-31 22:36:26 +0700

    create web traval

---
+++ script.js
@@ -0,0 +1,65 @@
+(function ($) {
+  var myBlog = [
+    {
+      title: "Car images",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, dolores!",
+      image: "img/card-1.jpg",
+      link: "#",
+    },
+    {
+      title: "Sunset images",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, dolores!",
+      image: "img/card-2.jpg",
+      link: "#",
+    },
+    {
+      title: "Landscape images",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, dolores!",
+      image: "img/card-3.jpg",
+      link: "#",
+    },
+    {
+      title: "People images",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, dolores!",
+      image: "img/card-4.jpg",
+      link: "#",
+    },
+    {
+      title: "Desert images",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, dolores!",
+      image: "img/card-5.jpg",
+      link: "#",
+    },
+    {
+      title: "Mountain images",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, dolores!",
+      image: "img/card-6.jpg",
+      link: "#",
+    },
+  ];
+  let blog = document.getElementById("MyListBlog");
+  let innerHTML = "";
+  for (let i = 0; i < myBlog.length; i++) {
+    let data = myBlog[i];
+    innerHTML += `
+        <li class="list-item">
+                <div class="thumb">
+                    <a href="${data.image}" class="lightbox">
+                        <img class="info-img" src="${data.image}" alt="">
+                    </a>
+                </div>
+                <h3 class="head-color before">${data.title}</h3>
+                <p>${data.text}</p>
+                <div class="colored-button-div">
+                    <a href="${data.link}" class="colored-button">View Home</a>
+                </div>
+            </li>
+            `;
+  }
+  blog.innerHTML = innerHTML;
+
+  $("#MyListBlog").magnificPopup({
+    delegate: "a",
+    type: "image",
+  });
+})(jQuery);
